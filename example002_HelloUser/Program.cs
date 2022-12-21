5629fcc37b04699b25c1a94ce1fcaa73eeda8198
shutnik42 <118187272+shutnik42@users.noreply.github.com>
2022-12-21 15:16:30 +0300

    Example002 HelloUser

---
+++ example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите Ваше имя ");
+string username = Console.ReadLine();
+Console.WriteLine("Шалом " + username);
+//Console.Write("Введите Ваше имя");
+//string username = Console.ReadLine();
+//Console.Write("Шалом " + username);
