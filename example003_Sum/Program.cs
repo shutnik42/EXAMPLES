7792d348d074fe524c31a6584f1848dd5a060676
shutnik42 <118187272+shutnik42@users.noreply.github.com>
2022-12-21 17:39:50 +0300

    add random

--- example003_Sum/Program.cs
+++ example003_Sum/Program.cs
@@ -1,5 +1,10 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿//int numberA = 31;
+//int numberB = 5;
 //Console.WriteLine(numberA + numberB);
-int result = numberA + numberB;
-Console.WriteLine(result);
+//int result = numberA + numberB;
+//Console.WriteLine(result);
+int numberA = new Random().Next(1,10);//1,2.3,4...9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
